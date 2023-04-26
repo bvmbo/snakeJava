b20dc561b5a14f6ba62dfdf0513e3b0d278491b2
bvmbo <wiktorsz2001@wp.pl>
2023-04-26 14:33:56 +0200

    changed drawing objects order

--- src/snake/GraphicsWIN.java
+++ src/snake/GraphicsWIN.java
@@ -28,22 +28,25 @@ public class GraphicsWIN extends Graphics{
             g2d.drawString("Press Any Key", (float) Game.width / 2 * Game.dimension - currentFont.getSize() * 1.4F - 35, (float) Game.height / 2 * Game.dimension - currentFont.getSize() * 1.4F);
         }
         else if(Objects.equals(state, "RUNNING")) {
-            g2d.setColor(Color.red);
+            g2d.setColor(Color.red); //ustawianie pozycji i koloru jabłka
             g2d.fillRect(f.getX() * Game.dimension, f.getY() * Game.dimension, Game.dimension, Game.dimension);
-            g2d.setColor(Color.WHITE);
-            g2d.setFont(newFont);
-            g2d.drawString("Score: " + (s.getBody().size() - 3), Game.dimension, Game.dimension);
-            g2d.drawString("Highscore " + game.highScore, Game.dimension * Game.dimension, Game.dimension);
 
             g2d.setColor(Color.green);
-            for(Rectangle r : s.getBody()) {
+            for(Rectangle r : s.getBody()) {    //ustawianie koloru ciała
                 g2d.fill(r);
             }
             g2d.setColor(darkGreen);
-            g2d.fill(s.getBody().get(0));
+            g2d.fill(s.getBody().get(0));   //ustawianie koloru głowy
+
+            g2d.setColor(Color.WHITE);
+            g2d.setFont(newFont);           //ustawianie napisu oraz jego koloru
+            g2d.drawString("Score: " + (s.getBody().size() - 3), Game.dimension, Game.dimension);
+            g2d.drawString("Highscore " + game.highScore, Game.dimension * Game.dimension, Game.dimension);
+
+
         }
         else {
-            g2d.setColor(Color.RED);
+            g2d.setColor(Color.RED);    //ustawianie koloru tekstu końcowego
             g2d.setFont(newFont);
             g2d.drawString("YOU LOST", Game.width/2 * Game.dimension - 40, Game.height / 2 * Game.dimension - 20);
 
