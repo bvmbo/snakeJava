8f0b8c7972a3789c1d365bfa622425ed53e88db8
bvmbo <wiktorsz2001@wp.pl>
2023-05-10 19:03:50 +0200

    food no longer spawns on level walls

--- src/snake/Food.java
+++ src/snake/Food.java
@@ -5,23 +5,38 @@ import java.awt.Rectangle;
 public class Food {
 	private int x;
 	private int y;
+	private final Wall wall = new Wall();
+	private final Rectangle[] mediumWall = wall.getWallMedium();
+	private final Rectangle[] hardWall = wall.getWallHard();
+	private final Rectangle f = new Rectangle(20,20);
 
 	public Food(){}
-	
 	public Food(Snake player) {
 		this.random_spawn(player);
 	}
-	
 	public void random_spawn(Snake player) {
 		boolean onSnake = true;
 		while(onSnake) {
 			onSnake = false;
-			
+
 			x = (int)(Math.random() * Game.width - 1);
 			y = (int)(Math.random() * Game.height - 1);
-			
-			for(Rectangle r : player.getBody()){ 
+			f.setLocation(x * 20, y * 20);
+
+			for(Rectangle r : player.getBody()){
 				if(r.x == x && r.y == y) {
+					onSnake = true;
+				}
+			}
+
+			for(Rectangle r : mediumWall){
+				if(r.getBounds().intersects(f)){
+					onSnake = true;
+				}
+			}
+
+			for(Rectangle r : hardWall){
+				if(r.getBounds().intersects(f)){
 					onSnake = true;
 				}
 			}
@@ -34,5 +49,9 @@ public class Food {
 
 	public int getY() {
 		return y;
+	}
+
+	public Rectangle getF(){
+		return f;
 	}
 }
